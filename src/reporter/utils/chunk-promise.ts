5248df6cb1001ee4fba927f2bd288c1bb4fccdcf
Igor Lizenko <igor.lizenko@eze.tech>
2025-04-18 23:14:56 +0300

    JSDOC added

--- src/reporter/utils/chunk-promise.ts
+++ src/reporter/utils/chunk-promise.ts
@@ -1,15 +1,29 @@
 import logger from '@logger';
 
-export async function resolvePromisesInChunks<R, T>({
+/**
+ * Processes an array of input data in chunks by applying a given async function to each element
+ * and resolving the promises in parallel within each chunk.
+ *
+ * @template T - Type of the input data elements
+ * @template R - Type of the output data elements
+ *
+ * @param {Object} params - The parameters object
+ * @param {T[]} params.arrayInputData - Array of input data to be processed
+ * @param {number} params.chunkSize - Size of each chunk to process in parallel
+ * @param {function(T): Promise<R|null>} params.functionToCall - Async function to apply to each input element
+ *
+ * @returns {Promise<R[]>} Array of resolved non-null results
+ */
+export async function resolvePromisesInChunks<T, R>({
     arrayInputData,
     chunkSize,
     functionToCall
 }: {
-    arrayInputData: R[],
+    arrayInputData: T[],
     chunkSize: number,
-    functionToCall: (input: R) => Promise<T | null>
-}): Promise<T[]> {
-    const results: T[] = [];
+    functionToCall: (input: T) => Promise<R | null>
+}): Promise<R[]> {
+    const results: R[] = [];
 
     const quantityOfChunks = Math.ceil(arrayInputData.length / chunkSize);
 
